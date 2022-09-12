1117ef0c4dba99952a32dccd85f5bffadd040010
elysdominguez <elysdominguez2@gmail.com>
2022-09-12 15:12:06 +0200

    connect my app with Redux

---
+++ src/store/balance/slice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  amount: 0,
+};
+
+export const balanceSlice = createSlice({
+  name: "balance",
+  initialState,
+  reducers: {
+    deposit: (state, action) => {
+      console.log("Hello from the reducer!");
+      state.amount = state.amount + action.payload;
+    },
+  },
+});
+
+export const { deposit } = balanceSlice.actions;
+export default balanceSlice.reducer;
